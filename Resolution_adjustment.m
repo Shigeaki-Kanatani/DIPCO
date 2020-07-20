7f942ebed255b0a4a07b45ae286d4d2b6781569b
Shigeaki-Kanatani <55106354+Shigeaki-Kanatani@users.noreply.github.com>
2020-07-20 12:32:08 +0200

    Delete Resolution_adjustment.m

--- Resolution_adjustment.m
+++
@@ -1,9 +0,0 @@
-function Resolution_adjustment(F_XYZIDint, out_dir)
-    
-    F_XYZIDint2 = F_XYZIDint;
-    F_XYZIDint2(:,3) = F_XYZIDint2(:,3)*5;
-    F_XYZIDint2(:,2) = F_XYZIDint2(:,2)*0.585;
-    F_XYZIDint2(:,1) = F_XYZIDint2(:,1)*0.585;
-    Header ={'X', 'Y', 'Z', 'ID', 'Intensity'};
-    csvwrite_with_headers([out_dir '\' 'F_XYZIDint2.csv'],F_XYZIDint2,Header);
-    %csvwrite([out_dir '\' 'F_XYZIDint2.csv'], F_XYZIDint2);
